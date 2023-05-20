395be02735197dc7a0a9bf07e2b5c35a1bbe004b
cpkopp12 <cameronpkopp@gmail.com>
2023-05-20 15:56:44 -0400

    match password validator

--- emals/src/app/auth/signup/signup.component.ts
+++ emals/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatchPassword } from '../validators/match-password';
 
 @Component({
   selector: 'app-signup',
@@ -8,9 +9,25 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class SignupComponent {
   authForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-    passwordConfirmation: new FormControl('')
-  });
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^[a-z0-9]+$/)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20)
+    ]),
+    passwordConfirmation: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20)
+    ])
+  }, 
+  { validators: [this.matchPassword.validate] });
+
+  constructor(private matchPassword: MatchPassword) {}
 
 }
